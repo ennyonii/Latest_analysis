8749bb5c332ab23a4c284c5f200d805257e9fbd9
ennyonii <eniolaoni30@gmail.com>
2026-04-23 18:22:23 -0600

    updates

---
+++ Jottings.R
@@ -0,0 +1,8 @@
+#Permanova model - adonis, Linear model & regression model - lm, Generalised linear model - glm
+#Anova model - aov, 2 way anova - aov(dependent ~two independent variables) Glmms - glmer
+#t test - t.test, bray curtis - vedgist(data, method = bray), NMDS - metaMDS(data, distance = "bray", autotransform = FALSE)
+#Diet composition, trophic plasticity (levins index, resource richness), biomass ratio - VARIABLES I CONSIDERED
+#Bray curtis- to measure dissimilarity between sites
+#NMDS- to create ordination plot for the results of the bray curtis
+#envfit - 
+
